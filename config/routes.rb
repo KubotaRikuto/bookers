d969ad4d7779b6393ff254f59549f8691e0dcfb3
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-24 10:50:24 +0000

    [Add] 3-13 appトップ画面のhtml更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'homes/top' # アプリケーショントップ画面
+  # top画面をルートパス設定にする
+  root to: 'homes#top' # アプリケーショントップ画面
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books #booksコントローラに紐づくルーティング(以下確認したルーティング)
     # index
     # create
-    # new
+    # newgit 
     # edit
     # show
     # update
