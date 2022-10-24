76ebae2e264c4e9a4fb4588caa30411a96c9dfdc
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-24 08:50:18 +0000

    [Add] 3-13 Bookモデルファイル追加 , booksテーブル作成 homes,booksコントローラ作成 ビューファイル追加 ルーティングファイル更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'homes/top' # アプリケーショントップ画面
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books #booksコントローラに紐づくルーティング(以下確認したルーティング)
+    # index
+    # create
+    # new
+    # edit
+    # show
+    # update
+    # destroy
 end
