b855e826d2cbca8e10bf11ad4b49b69f702ab5f1
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-24 17:25:51 +0000

    [Add] 3-13 新規投稿フォーム作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new # 空のオブジェクト作成
   end
-  
+
   def create
+    # データを受け取り新規登録
+    book = Book.new(book_params)
+    # データをdbに保存
+    book.save
+    # フラッシュメッセージを定義 -> 詳細画面へリダイレクト
+    # バリデーション設定時に導入予定
+    # 詳細画面にリダイレクト
+    redirect_to book_path(book.id)
   end
-   
+
   def index
   end
 
@@ -12,5 +21,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(book_params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  # ストロングパラメータ
+  # book_params -> 保存したデータの絞り込みを行う
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
