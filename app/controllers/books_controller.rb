4e1f320e28e17022e45ed9218a12cf0fa58ae677
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-25 13:33:30 +0000

    [Add] 3-13 ルーティング,コントローラ,index,show,edit作成 css未実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,46 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new # 空のオブジェクト作成
   end
 
   def create
     # データを受け取り新規登録
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # データをdbに保存
-    book.save
-    # フラッシュメッセージを定義 -> 詳細画面へリダイレクト
-    # バリデーション設定時に導入予定
-    # 詳細画面にリダイレクト
-    redirect_to book_path(book.id)
+    if @book.save
+      # フラッシュメッセージを定義 -> 詳細画面へリダイレクト
+      flash[:notice] = "Book was successfully created."
+      # 詳細画面にリダイレクト
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all # index表示時のレコードを取得
+      render :index # ":アクション名"のviewを表示
+    end
   end
 
   def index
-    @books = Book.all
+    @book = Book.new # 空のオブジェクト作成 投稿フォーム用
+    @books = Book.all #一覧表示用
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(book_params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was sccessfully updated."
+    redirect_to book_path(book.id)
   end
 
   def destroy
-    book = Book.find(book_params[:id])
+    book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was successfully destroyed."
+    flash[:notice] = "Book was successfully destroy."
     redirect_to books_path
   end
 
